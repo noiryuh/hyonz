f7b84ab6d777ec1a1cbc19767129a1bed79476ef
noiryuh <noiryuh@proton.me>
2022-09-23 22:31:00 +0700

    fix `skdPath` not working

--- build.zig
+++ build.zig
@@ -18,7 +18,7 @@ pub fn build(b: *std.build.Builder) void {
 
 /// Return absolute path from this project's root directory which containing this 'build.zig'
 pub fn sdkPath(comptime path: []const u8) []const u8 {
-    if (std.fs.path.isSep(path[0])) {
+    if (comptime std.fs.path.isSep(path[0])) {
         @compileError("'" ++ path ++ "' must be a relative path");
     }
     const root_dir = comptime std.fs.path.dirname(@src().file) orelse ".";
