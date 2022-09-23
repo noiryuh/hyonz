78bc08ca8aa0dce41b296a84c4c1381ba5ee6cca
noiryuh <noiryuh@proton.me>
2022-09-23 22:29:47 +0700

    add 'test' step for build.zig

--- build.zig
+++ build.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    _ = b;
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const main_tests = b.addTest("src/main.zig");
+    main_tests.setTarget(target);
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
 }
 
 //==============================================
